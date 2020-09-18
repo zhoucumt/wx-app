8fa1dabebe78c7e67a7bda79d2dcaeca6585a750
zhoucumt <553817478@qq.com>
2020-09-18 22:11:27 +0800

    生命周期顺序onLoad->onShow->onReady

--- pages/wxml/index.js
+++ pages/wxml/index.js
@@ -5,42 +5,42 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    time: (new Date()).toString()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad');
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    console.log('onReady');
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    console.log('onShow');
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide');
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload');
   },
 
   /**
